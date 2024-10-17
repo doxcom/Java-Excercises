6e01d2fed4d232de50b5a877d455067a4010fc0f
doxcom <aldoperez1319@gmail.com>
2024-10-17 16:57:00 -0600

    Palindromo resuelto

--- Palindrome/PalindromeOrNot.java
+++ Palindrome/PalindromeOrNot.java
@@ -4,11 +4,15 @@ public class PalindromeOrNot {
 
     public boolean isPalindrome(String word) {
 
-        String match="^[A-Za-z0-9_.]+$";
-        if(word.isEmpty()) return false;
 
-        for(int i=0, j= word.length()-1; i<j; i++,j--){
-            if(word.charAt(i) != word.charAt(j) || !word.matches(match)){
+        if(word==null || word.isEmpty() || word.startsWith(" ")) return false;
+
+        String word2=word.toLowerCase().replace(" ","").replace("\t","").replace("\r","");
+        System.out.println(word);
+        System.out.println("word without spaces and special characters:  " + word2);
+        
+        for(int i=0, j= word2.length()-1; i<j; i++,j--){
+            if(word2.charAt(i) != word2.charAt(j)){
                 return false;
             }
         }
@@ -16,8 +20,8 @@ public class PalindromeOrNot {
     }
 
     public static void main(String []args){
+        System.out.println(new PalindromeOrNot().isPalindrome(" \t\t\r \t "));
 
-
-        System.out.println(new PalindromeOrNot().isPalindrome("a"));
     }
 }
+
