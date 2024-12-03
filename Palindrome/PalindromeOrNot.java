225c7b5aeb060ac1140831edc2ce93b29b30e8ed
doxcom <aldoperez1319@gmail.com>
2024-12-02 19:59:07 -0600

    sustituir funciones lower case y replace de java con algoritmo propio

--- Palindrome/PalindromeOrNot.java
+++ Palindrome/PalindromeOrNot.java
@@ -7,12 +7,12 @@ public class PalindromeOrNot {
 
         if(word==null || word.isEmpty() || word.startsWith(" ")) return false;
 
-        String word2=word.toLowerCase().replace(" ","").replace("\t","").replace("\r","");
-        System.out.println(word);
-        System.out.println("word without spaces and special characters:  " + word2);
+     //   String word2=word.toLowerCase().replace(" ","").replace("\t","").replace("\r","");
+      //  System.out.println(word);
+       // System.out.println("word without spaces and special characters:  " + word2);
 
-        for(int i=0, j= word2.length()-1; i<j; i++,j--){
-            if(word2.charAt(i) != word2.charAt(j)){
+        for(int i=0, j= word.length()-1; i<j; i++,j--){
+            if(word.charAt(i) != word.charAt(j)){
                 return false;
             }
         }
